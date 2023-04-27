fb706d0516051d597fb56b305b00312e22d61df2
pmihango <pmihango@gmail.com>
2023-04-27 08:24:28 +0300

    fixed the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,24 @@ import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 
 import { styles} from '../styles'
-import { navLink } from '../constants'
+import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] =useState('')
   return (
-    <nav>
-      className={'${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary'}
+    <nav
+      className={'${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         
-        <Link>
-        {/* top of the page */}
-        to="/" 
-        className="flex items-center gap-2"
-        {/* Keep track of where we are on the page */}
-        onClick={()=>{
+        <Link
+         to="/" className="flex items-center gap-2"
+          onClick={()=> {
           setActive("");
           window.scroll(0,0);
-          <img src="{logo}" alt="logo" className='w-9 h-9 object-contain' />
-        }}
+          }}
+        >
+          <img src="{logo}" alt="logo" className="w-9 h-9 object-contain" />
         </Link>
 
       </div>
