64d90b59f5ab3c66d57936535674be0a892b962b
pmihango <pmihango@gmail.com>
2023-04-27 22:22:06 +0300

    Added the hero section to homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,16 @@ const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={'${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5'} >
-        <div className='flex fle-col justify-center items-center mt-5'>
+        <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 h-5 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={'${styles.heroHeadText} text-white'}> Hi, I am <span className='text-[#915eff]'> Paul</span></h1>
+          <p className={'${styles.heroSubText} mt-2 text-white-100'} >
+            I am a Software Engineer with a passion for front end development.
 
+          </p>
         </div>
       </div>
     
