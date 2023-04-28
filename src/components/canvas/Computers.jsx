5522e42787fcc344a8494ae35da4893a0a022d33
pmihango <pmihango@gmail.com>
2023-04-28 19:38:16 +0300

    added the animatted computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,17 +1,36 @@
-import { Suspense, useEffect, useState} from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-import CanvasLoader from '../Loader'
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene')
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black"/>
-
+      <pointLight intensity={1}/>
+      <primitive object={computer.scene}
+      scale={0.75}
+      position={[0, -3.25, -1.5]}
+      rotation={[-0.01, -30.2, -0.1]}       
+      />
     </mesh>
   )
 }
-
+const ComputersCanvas = () => {
+  return(
+    <Canvas frameloop="demand"
+    shadows
+    camera={{ position: [20,3,5], fov: 25}}
+    gl={{ preserveDrawingBuffer: true}}    
+    >
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
+        <Computers/>
+      </Suspense>
+      <Preload all/>
+    </Canvas>
+  )
+}
 export default Computers
